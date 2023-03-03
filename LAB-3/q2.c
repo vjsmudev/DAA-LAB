6164255f1ec9b0f9998a335c4d327968e5314791
dev_0419 <52438900+vjsmudev@users.noreply.github.com>
2023-03-03 22:18:51 +0530

    Update q2.c

--- LAB-3/q2.c
+++ LAB-3/q2.c
@@ -4,37 +4,49 @@ efficiency.*/
 #include<stdio.h>
 #include<string.h>
 
-int main(){
-    char txt[50];
-    char pat[50];
-    printf("Enter the text:\n");
-    gets(txt);
-    printf("Enter the pattern text:\n");
-    gets(txt);
-    int m,n,opc,j,i;
-    opc = 0;
-    m = strlen(pat);
-    n = strlen(txt);
-    printf("String length of text: %d\n",m);
-    printf("String length of pattern: %d\n",m);
-    
-    /* A loop to slide pat[] one by one */
-    for (i = 0; i <= n - m; i++){
-        /* For current index i, check for pattern match */
-        for(j = 0; j < m; j++){
-            // the basic operation is the comparision between characters in the text and the pattern
-            opc++;
-            if(txt[i + j] != pat[j]){
+void search(char* pat, char* txt){
+    int m = strlen(pat);
+    int n = strlen(txt);
+    int opc = 0;
+//loop to slide past the pattern one by one
+// n-m maximum possible starting position of the string without exceeding the end of the text string
+    for (int i = 0; i < n - m; i++){
+       
+        int j;
+
+        //For current index i check for the pattern match
+        for (j = 0; j < m; j++){
+             opc++;
+//Checks if the current character in the  txt[i+j] is not equal to the current character in the text string 
+
+/*For example, if the pattern string is "abc" and the text string is "defgabcxyz",
+ then the loop would iterate through 
+ starting positions 0, 1, 2, 3, and 4,
+  because starting at position 5 would 
+  only leave the characters "a", "b", and "c" outside the end of the text string.*/
+            if (txt[i+j] != pat[j]){
                 break;
             }
         }
-        if(j == m){
-            printf("Pattern found at index %d\n",i);
+        /*This if statement checks if the inner loop completed iterating 
+        through the entire string pattern without finding mismatchse*/
+            if (j == m){
+                printf("Pattern found at index %d",i);
+            }
         }
-        else{
-            printf("Operation count: %d\n",opc);
-        }
-    }   
-    return 0;
-}
+        printf("Operation count:%d\n",opc);
+    }
 
+    int main(){
+        char txt[100];
+        char pat[100];
+        
+        printf("Enter the text string:\n");
+        scanf(" %[^\n]s ", txt);
+
+        printf("Enter the pattern string:\n");
+        scanf(" %[^\n]s ", pat);
+
+        search(pat,txt);
+        return 0;
+    }
